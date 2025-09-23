a7ba92aa217d78698b96bcce39801ee6ff07276e
SrCiocco <tenpeny10@gmail.com>
2025-09-23 18:15:13 -0300

    Ejercicio 1 parte 1 terminado.

--- Programacion-2/estructuras/struct-tiempos/main.c
+++ Programacion-2/estructuras/struct-tiempos/main.c
@@ -13,84 +13,90 @@ typedef struct tiempo {
 
 
 
-void crear_tiempos(Tiempo *t1, Tiempo *t2);/* Definimos las structs t1 y t2. */
+void crear_tiempo(Tiempo *t); /* Definimos las structs t1 y t2. */
 
 int compara_tiempos(Tiempo *t1, Tiempo *t2);
+
+void imprime_tiempo(Tiempo t);
 
 int main()
 {
 	Tiempo t1, t2;
 
-	crear_tiempos(&t1, &t2);
+	printf("\t -- Tiempo 1 --\n");
+	crear_tiempo(&t1);
+
+	printf("\t -- Tiempo 2 --\n");
+	crear_tiempo(&t2);
+
 	int compara = compara_tiempos(&t1, &t2);
-	printf("Año: %d\nMes: %d\nDia: %d\nHora: %d\nMinuto: %d\nSegundo: %d\n", t1.anio, t1.mes, t1.dia, t1.hora, t1.minuto, t1.segundo);
+	
 	printf("\n");
-	printf("Año: %d\nMes: %d\nDia: %d\nHora: %d\nMinuto: %d\nSegundo: %d\n", t2.anio, t2.mes, t2.dia, t2.hora, t2.minuto, t2.segundo);
+
+	imprime_tiempo(t1);
+
+	imprime_tiempo(t2);
 
 	printf("\n");
-	printf("%d", compara);
 
+	if (compara == ANTERIOR) {
+		printf("Tiempo 1 es anterior a Tiempo 2.\n");
+
+	} else if (compara == POSTERIOR) {
+		printf("Tiempo 1 es posterior a Tiempo 2.\n");
+
+	} else {
+		printf("Tiempo 1 es igual a Tiempo 2\n");
+	}
+
+	return 0;
 }
 
-void crear_tiempos(Tiempo *t1, Tiempo *t2)
+void crear_tiempo(Tiempo *t)
 {
-	printf("\t-- Tiempo 1 --");
-	
 	printf("\nIntroduce el anio: ");
-	scanf("%d", &t1->anio);
+	scanf("%d", &t->anio);
 	
 	printf("\nIntroduce el mes: ");
-	scanf("%d", &t1->mes);
+	scanf("%d", &t->mes);
 	
 	printf("\nIntroduce el dia: ");
-	scanf("%d", &t1->dia);
+	scanf("%d", &t->dia);
 
-	printf("\nIntroduce el hora: ");
-	scanf("%d", &t1->hora);
-
-	printf("\nIntroduce el minuto: ");
-	scanf("%d", &t1->minuto);
-
-	printf("\nIntroduce el segundo: ");
-	scanf("%d", &t1->segundo);
-
-	printf("\t-- Tiempo 2 --");
-	
-	printf("\nIntroduce el anio: ");
-	scanf("%d", &t2->anio);
-	
-	printf("\nIntroduce el mes: ");
-	scanf("%d", &t2->mes);
-	
-	printf("\nIntroduce el dia: ");
-	scanf("%d", &t2->dia);
-
-	printf("\nIntroduce el hora: ");
-	scanf("%d", &t2->hora);
+	printf("\nIntroduce la hora: ");
+	scanf("%d", &t->hora);
 
 	printf("\nIntroduce el minuto: ");
-	scanf("%d", &t2->minuto);
+	scanf("%d", &t->minuto);
 
 	printf("\nIntroduce el segundo: ");
-	scanf("%d", &t2->segundo);
+	scanf("%d", &t->segundo);
 }
 
 int compara_tiempos(Tiempo *t1, Tiempo *t2)
-{	/* Nos encargamos del año. */
-	if (t1->anio < t2->anio) return ANTERIOR;
-	else if (t1->anio > t2->anio) return POSTERIOR;
+{
+	if (t1->anio != t2->anio)
+		return (t1->anio < t2->anio) ? ANTERIOR : POSTERIOR;
 
-	if (t1->anio == t2->anio) {
-		if (t1->mes == t2->mes) {
-			if (t1->dia == t2->dia) {
-				if (t1->hora == t2->hora) {
-					if (t1->minuto == t2->minuto) {
-						if (t1->segundo == t2->segundo) {
-							return IGUAL;
-						}
-					}
-				}
-			}
-		}
-	}
+	if (t1->mes != t2->mes)
+		return (t1->mes < t2->mes) ? ANTERIOR : POSTERIOR;
+
+	if (t1->dia != t2->dia) 
+		return (t1->dia < t2->dia) ? ANTERIOR : POSTERIOR;
+
+	if (t1->hora != t2->hora) 
+		return (t1->hora < t2->hora) ? ANTERIOR : POSTERIOR;
+
+	if (t1->minuto != t2->minuto) 
+		return (t1->minuto < t2->minuto) ? ANTERIOR : POSTERIOR;
+
+	if (t1->segundo != t2->segundo) 
+		return (t1->segundo < t2->segundo) ? ANTERIOR : POSTERIOR;
+
+	return IGUAL;
+}
+
+void imprime_tiempo(Tiempo t)
+{
+	printf("%d/%d/%d %d:%d:%d\n", t.dia, t.mes, t.anio, t.hora, t.minuto, t.segundo);
 }
